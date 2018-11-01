aecce66b500d9d72c2feffa79b95c5c2cc828ddf
Kelvin <kelvinwong0922@gmail.com>
2018-11-01 16:01:48 -0700

    Setup Google Oauth Callback function

--- routes/auth.js
+++ routes/auth.js
@@ -9,4 +9,13 @@ router.get(
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect("/dashboard");
+  }
+);
+
 module.exports = router;
