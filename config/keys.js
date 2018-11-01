89c238fc3d600198b92ee4bb12c72548f828c6e4
Kelvin <kelvinwong0922@gmail.com>
2018-10-31 17:30:10 -0700

    Created Google OAuth API

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "",
+  googleClientID:
+    "987054282984-f2e6o02tek1t5j5la6tnvn12vgm4ggg7.apps.googleusercontent.com",
+  googleClientSecret: "6faCMlQb1szNoEivOjVAcUOU"
+};
