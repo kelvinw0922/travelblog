834b1b3e7849a1537a5cb9b77030642502f061cd
Kelvin <kelvinwong0922@gmail.com>
2018-10-31 17:31:06 -0700

    Created MongoDB URI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-  mongoURI: "",
+  mongoURI:
+    "mongodb://rozanate:tracymacno1@ds039165.mlab.com:39165/travelblog-dev",
   googleClientID:
     "987054282984-f2e6o02tek1t5j5la6tnvn12vgm4ggg7.apps.googleusercontent.com",
   googleClientSecret: "6faCMlQb1szNoEivOjVAcUOU"
