a98186c11d76b7c57d76dff0e84d316f2bad9732
Kelvin <kelvinwong0922@gmail.com>
2018-11-06 16:47:32 -0800

    Created Route Protection routers to restrict unauthorized access

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuthenticated } = require("../helpers/auth");
 
 router.get("/", (req, res) => {
   res.render("blogs/index");
 });
 
 // Add Blog Form
-router.get("/add", (req, res) => {
+router.get("/add", ensureAuthenticated, (req, res) => {
   res.render("blogs/add");
 });
 
