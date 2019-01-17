32d5a290fa59b9d29c67718dc7df86d737eed690
Kelvin Wong <kelvinwong0922@gmail.com>
2019-01-17 13:34:01 -0800

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://rozanate:tracymacno1@ds039165.mlab.com:39165/travelblog-dev",
-  googleClientID:
-    "987054282984-f2e6o02tek1t5j5la6tnvn12vgm4ggg7.apps.googleusercontent.com",
-  googleClientSecret: "6faCMlQb1szNoEivOjVAcUOU"
-};
